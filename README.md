2024677efaf871ccfb84f8360604bbe268b785d0
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:20:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# More Comments
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/moreComments?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-other-lightgrey.svg)
+
+Allows you to add comments in the form of blocks to increase program readability.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,67 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'moreComments.name': 'More Comments',
+            'moreComments.description': 'Allows you to add comments in the form of blocks to increase program readability.'
+        },
+        'ru': {
+            'moreComments.name': 'More Comments',
+            'moreComments.description': 'Allows you to add comments in the form of blocks to increase program readability.'
+        },
+        'zh-cn': {
+            'moreComments.name': 'More Comments',
+            'moreComments.description': 'Allows you to add comments in the form of blocks to increase program readability.'
+        },
+        'zh-tw': {
+            'moreComments.name': 'More Comments',
+            'moreComments.description': 'Allows you to add comments in the form of blocks to increase program readability.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'MORECOMMENTS_CATEGORY': 'More Comments',
+            'MORECOMMENTS_COMMANDBLOCK': 'comments: %1',
+            'MORECOMMENTS_BRANCHBLOCK': 'comments: %1'
+        },
+        'ru': {
+            'MORECOMMENTS_CATEGORY': 'More Comments',
+            'MORECOMMENTS_COMMANDBLOCK': 'comments: %1',
+            'MORECOMMENTS_BRANCHBLOCK': 'comments: %1'
+        },
+        'zh-cn': {
+            'MORECOMMENTS_CATEGORY': 'More Comments',
+            'MORECOMMENTS_COMMANDBLOCK': 'comments: %1',
+            'MORECOMMENTS_BRANCHBLOCK': 'comments: %1'
+        },
+        'zh-tw': {
+            'MORECOMMENTS_CATEGORY': 'More Comments',
+            'MORECOMMENTS_COMMANDBLOCK': 'comments: %1',
+            'MORECOMMENTS_BRANCHBLOCK': 'comments: %1'
+        }
+    }
+};
